4d4cceff1030f483463241f2cc7e24b2725a0ff6
hakim <hakim@freebsd.hakim.com>
2014-09-12 20:30:11 +0100

    - send goal breaking news by email to given recipient

--- goal.py
+++ goal.py
@@ -1,15 +1,44 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Get football(soccer) news from goal.com
-# using lxml & requests python modules
+# using lxml & requests python modules,
+# and send them by email using ssmtp
+# to recipient given as cmdline arg.
 # author h4k1m
 
 from lxml import html
+from datetime import datetime
 import requests
+import sys
+import socket
+import os
 
-page = requests.get('http://www.goal.com/en/module/breaking-news/content')
+# get email recipient (cmdline arg) & sender (hostname)
+if len(sys.argv) == 1:
+    print 'Please give recipient of the email!'
+    exit()
+sender = socket.gethostname()
+recipient = sys.argv[1]
+
+# get breaking news from goal.com
+page = requests.get('http://www.goal.com/de/module/breaking-news/content')
 tree = html.fromstring(page.text)
 times = tree.xpath('//time/text()')
 titles = tree.xpath('//span[@class="title"]/text()')
 
+# fill email file header
+email_file = '/home/hakim/Code/Python/node-scraping/email.txt'
+f = open(email_file, 'w')
+f.write('To: %s\n' % recipient)
+f.write('From: %s\n' % sender)
+f.write('Subject: Goal.com Breaking-news [%s]\n\n' % datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+# write news to file
 for (time, title) in zip(times, titles):
-    print "- %s: %s." % (time, title)
+    f.write("- %s: %s.\n" % (time.encode('utf8'), title.encode('utf8')))
+f.close()
+
+# send news file by email
+os.system('/usr/local/sbin/ssmtp %s < %s' % (recipient, email_file))
+print 'Goal.com Breaking-news have been sent'
